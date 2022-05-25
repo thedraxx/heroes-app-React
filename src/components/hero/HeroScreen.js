37f6c008e3946e0a256c6123a8a736703112f654
Frank <55867157+thedraxx@users.noreply.github.com>
2022-05-25 15:12:40 -0300

    added style HeroScreen

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useParams, Navigate } from 'react-router-dom'
+import { useParams, Navigate, useNavigate } from 'react-router-dom'
 import { getHeroById } from '../selectors/getHeroById';
 
 export const HeroScreen = () => {
@@ -15,12 +15,51 @@ export const HeroScreen = () => {
     return <Navigate to='/' />
   }
 
+  const imgPath = `/assets/${hero.id}.jpg`;
+
+  const navigate = useNavigate();
+
+  const handleReturn = (e) => {
+    e.preventDefault()
+    if (hero.publisher === 'Marvel Comics') {
+      navigate('/marvel')
+    } else {
+      navigate('/dc')
+    }
+  }
+
   return (
-    <div>
-      <h1>HeroScreen</h1>
-      <p>
-        {hero.superhero}
-      </p>
+    <div className='row mt-5'>
+      <div className='col-4'>
+        <img
+          src={imgPath}
+          alt={hero.superhero}
+          className="img-thumbnail"
+        />
+      </div>
+      <div className='col-8'>
+        <h3>{hero.superhero}</h3>
+        <ul className='list-group list-group-flush'>
+          <li className='list-group-item'>
+            <b>Alter ego: </b>{hero.alter_ego}
+          </li>
+          <li className='list-group-item'>
+            <b>publisher: </b>{hero.publisher}
+          </li>
+          <li className='list-group-item'>
+            <b>First appearance: </b>{hero.first_appearance}
+          </li>
+        </ul>
+        <h5 className='mt-3'>Characters</h5>
+        <p>{hero.characters}</p>
+        <button
+          className='btn btn-outline-info'
+          onClick={handleReturn}
+        >
+          regresar
+        </button>
+
+      </div>
     </div>
   )
 }
