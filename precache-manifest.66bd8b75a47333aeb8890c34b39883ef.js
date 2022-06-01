526a6191a5cc9340568e5bb71f3ccdfeebf6e7a8
Frank <55867157+thedraxx@users.noreply.github.com>
2022-06-01 15:15:34 -0300

    Updates

---
+++ precache-manifest.66bd8b75a47333aeb8890c34b39883ef.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "3cf86a2e44e01b53f8ad",
+    "url": "/heroes-app-React/static/css/main.38688c56.chunk.css"
+  },
+  {
+    "revision": "3cf86a2e44e01b53f8ad",
+    "url": "/heroes-app-React/static/js/main.3cf86a2e.chunk.js"
+  },
+  {
+    "revision": "6d901b3e33806cc9ca52",
+    "url": "/heroes-app-React/static/js/1.6d901b3e.chunk.js"
+  },
+  {
+    "revision": "3badb205084d4f64d3ac",
+    "url": "/heroes-app-React/static/js/2.3badb205.chunk.js"
+  },
+  {
+    "revision": "867334458ad22eea9da4",
+    "url": "/heroes-app-React/static/js/runtime~main.86733445.js"
+  },
+  {
+    "revision": "a179d8711d59015e5d3d53637ad9fce7",
+    "url": "/heroes-app-React/static/media/dc-arrow.a179d871.jpg"
+  },
+  {
+    "revision": "8efedfa7bf353070fb1dc56bc36a8bd3",
+    "url": "/heroes-app-React/static/media/dc-batman.8efedfa7.jpg"
+  },
+  {
+    "revision": "5d203be297ee7aee6df388f48effc2d2",
+    "url": "/heroes-app-React/static/media/dc-black.5d203be2.jpg"
+  },
+  {
+    "revision": "871a00dda57a453a698369e1687db44e",
+    "url": "/heroes-app-React/static/media/dc-blue.871a00dd.jpg"
+  },
+  {
+    "revision": "d22cd5d0cdbb8e6cb64ea844fd1082fa",
+    "url": "/heroes-app-React/static/media/dc-flash.d22cd5d0.jpg"
+  },
+  {
+    "revision": "dae0db0a19c068035a119a4cf07bfd39",
+    "url": "/heroes-app-React/static/media/dc-green.dae0db0a.jpg"
+  },
+  {
+    "revision": "9629381c9ae8fe6a5c9ab23308cd76f0",
+    "url": "/heroes-app-React/static/media/dc-martian.9629381c.jpg"
+  },
+  {
+    "revision": "3b3b7dd1a4716a9f8eec3c13c0611726",
+    "url": "/heroes-app-React/static/media/dc-robin.3b3b7dd1.jpg"
+  },
+  {
+    "revision": "ad88dd859f1b6cb05d235edfdf7d2da9",
+    "url": "/heroes-app-React/static/media/dc-superman.ad88dd85.jpg"
+  },
+  {
+    "revision": "b01172005be3eba0ddbe50bd07d3ece8",
+    "url": "/heroes-app-React/static/media/dc-wonder.b0117200.jpg"
+  },
+  {
+    "revision": "71c5a519e43cab5350b5a5dbb45f4ece",
+    "url": "/heroes-app-React/static/media/marvel-captain.71c5a519.jpg"
+  },
+  {
+    "revision": "017af60c9d646181629f0887c02e8ef0",
+    "url": "/heroes-app-React/static/media/marvel-cyclops.017af60c.jpg"
+  },
+  {
+    "revision": "3d4c3df826dce8f0a85e8e49bd3fb204",
+    "url": "/heroes-app-React/static/media/marvel-daredevil.3d4c3df8.jpg"
+  },
+  {
+    "revision": "df41e28ce57042715584e972ea53e5a2",
+    "url": "/heroes-app-React/static/media/marvel-hawkeye.df41e28c.jpg"
+  },
+  {
+    "revision": "8f5cd9397468b7b508ffa33b00c5014e",
+    "url": "/heroes-app-React/static/media/marvel-hulk.8f5cd939.jpg"
+  },
+  {
+    "revision": "8f307eef75cd45b3e7076ece1b368e5f",
+    "url": "/heroes-app-React/static/media/marvel-iron.8f307eef.jpg"
+  },
+  {
+    "revision": "c055e4a00bff140e8faf40eee0701eaa",
+    "url": "/heroes-app-React/static/media/marvel-silver.c055e4a0.jpg"
+  },
+  {
+    "revision": "9967f816d7adb4c674f99d230e9472c7",
+    "url": "/heroes-app-React/static/media/marvel-spider.9967f816.jpg"
+  },
+  {
+    "revision": "9f1b74e51e318f14c533fdb3b03a7421",
+    "url": "/heroes-app-React/static/media/marvel-thor.9f1b74e5.jpg"
+  },
+  {
+    "revision": "a1a41d9adf2f16f1220723a70ce20215",
+    "url": "/heroes-app-React/static/media/marvel-wolverine.a1a41d9a.jpg"
+  },
+  {
+    "revision": "d5b5904f270b3308a5de84f79b887cb5",
+    "url": "/heroes-app-React/index.html"
+  }
+];
